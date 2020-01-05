e136d1309656e1401968c176549fac43442afa98
Ra <felx90@outlook.com>
2020-01-04 21:08:18 -0800

    added a json web token sign for user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 const User = require('../../modals/User');
 // @route POST api/users
@@ -51,10 +53,23 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      -(
-        // Return jsonwebtok
 
-        res.send('User registered')
+      // Return jsonwebtok
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
       );
     } catch (err) {
       console.error(err.message);
